0958c6b161785512dbb7624dde63ba98945a6e8e
Sofiermas <Volserma@yandex.ru>
2024-05-12 15:13:12 +0300

    feat: add btn-scroll-up

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* --------------------- Burger menu --------------------- */
 const btnOpenMenu = document.querySelector('.header__logo-menu');
 const btnCloseMenu = document.querySelector('.header__nav-btn-close');
 const burgerMenu = document.querySelector('.header__nav');
@@ -8,3 +9,42 @@ function toggleBurgerMenu() {
 
 btnOpenMenu.addEventListener('click', toggleBurgerMenu)
 btnCloseMenu.addEventListener('click', toggleBurgerMenu)
+
+
+/* --------------------- Button scroll-up --------------------- */
+const offset = 700;
+const btnScrollUp = document.querySelector('.btn-scroll-up');
+const btnScrollUpSvgPath = document.querySelector('.btn-scroll-up__svg-path');
+const pathLength = btnScrollUpSvgPath.getTotalLength();
+
+btnScrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+btnScrollUpSvgPath.style.transition = 'stroke-dashoffset 30ms';
+
+// function button click
+btnScrollUp.addEventListener('click', () => {
+   window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+   });
+})
+
+// get length scroll page
+const getScroll = () => window.pageYOffset || document.documentElement.scrollTop;
+
+// update Svg-path shadow
+const updateShadowSvg = () => {
+   const heightScroll = document.documentElement.scrollHeight - window.innerHeight;
+   const dashoffset = pathLength - (getScroll() * pathLength / heightScroll);
+
+   btnScrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+// function add/remove class--active
+window.addEventListener('scroll', () => {
+   updateShadowSvg();
+   if (getScroll() > offset) {
+      btnScrollUp.classList.add('btn-scroll-up--active');
+   } else {
+      btnScrollUp.classList.remove('btn-scroll-up--active');
+   }
+})
